958e92dbd6c827d18151f4d20baea8ad5d1bb52e
mohaghhjj <129596788+mohaghhjj@users.noreply.github.com>
2023-04-02 14:52:56 +0430

    Add files via upload

--- SSB.py
+++ SSB.py
@@ -13,14 +13,14 @@ except:
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('Sarfraz.so'):
-        os.system('curl -L https://github.com/mohaghhjj/FT2/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
+        os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
         import Sarfraz
     else:
         import Sarfraz
 
 elif bit == '32bit':
     if not os.path.isfile('Sarfraz32.so'):
-        os.system('curl -L https://github.com/mohaghhjj/FT2/blob/main/Sarfraz32.cpython-311.so?raw=true -o Sarfraz32.so') 
+        os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz32.cpython-311.so?raw=true -o Sarfraz32.so') 
         import Sarfraz32
     else:
         import Sarfraz32
